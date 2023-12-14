d4cb4993a54705985811396eddb39148ebb6d310
smazhar <salman137@hotmail.com>
2023-12-14 08:37:08 +0400

    fileupload

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,9 @@
 export const environment = {
     production:false,
-    domain:"http://192.168.105.27:4222/",
+  //  domain:"http://192.168.105.27:4222/",
+   domain:"http://ajes-webapp.ajes.ae:4222/",
    
-   // domain:"https://Localhost:44359/",
+  //  domain:"https://Localhost:44359/",
    
-   // d
+
 };
